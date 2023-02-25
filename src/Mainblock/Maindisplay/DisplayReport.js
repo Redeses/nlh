5c378823ff294fbdabe3669720858c346c2068e0
Redeses <misseka13@gmail.com>
2023-02-25 17:53:43 +0200

    Update DisplayReport.js

--- src/Mainblock/Maindisplay/DisplayReport.js
+++ src/Mainblock/Maindisplay/DisplayReport.js
@@ -11,18 +11,30 @@ export default class DisplayReport extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            answerArray:DataHandler.getInstance().getAnswerArray()
+            answerArray:DataHandler.getInstance().getAnswerArray(),
+            reportArray:[]
 
         }
     }
 
     componentDidMount(){
-
+        this.makeAReport()
     }
 
     //makes a report based on the answer array held in DataHandler
     makeAReport=()=>{
-        
+        var proxyArray=[]
+        var x=1;
+        this.state.answerArray.forEach(element => {
+            if(element.length>1){
+                proxyArray.push(<div className='reportElement'>
+                    <h2 className='reportEleText'>For question {x}: answered = {element[0]}: which equalled {element[1]} points </h2>
+                </div>)
+                x++;
+            }
+            
+        });
+        this.setState({reportArray:proxyArray})
     }
 
     
@@ -32,7 +44,7 @@ export default class DisplayReport extends React.Component{
     render(){
     return (
         <div className='display_container'>
-            
+            {this.state.reportArray}
         </div>
 
     )}
